463abc5e9692eeb7a3c64c89ca340def2a64dfa6
zamorai <35697970+zamorai@users.noreply.github.com>
2020-09-17 17:29:25 -0400

    Updates

---
+++ precache-manifest.034f2ce1479d68a19eba6df5a041993f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174b1cbd69dd854d4e1c0cf7fa299d99",
+    "url": "/sharithg/senior-design-SW_miniproject.git/index.html"
+  },
+  {
+    "revision": "faadac7a8a7195c81a77",
+    "url": "/sharithg/senior-design-SW_miniproject.git/static/css/main.297f99ac.chunk.css"
+  },
+  {
+    "revision": "9bce0cd0c826a7c58637",
+    "url": "/sharithg/senior-design-SW_miniproject.git/static/js/2.c4bf8871.chunk.js"
+  },
+  {
+    "revision": "6c661bfd12b151c09a654fe75d1650f7",
+    "url": "/sharithg/senior-design-SW_miniproject.git/static/js/2.c4bf8871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faadac7a8a7195c81a77",
+    "url": "/sharithg/senior-design-SW_miniproject.git/static/js/main.eaafa2a9.chunk.js"
+  },
+  {
+    "revision": "d33229fc3289da46990d",
+    "url": "/sharithg/senior-design-SW_miniproject.git/static/js/runtime-main.f3396d09.js"
+  },
+  {
+    "revision": "d8d37e8089341e3eb71490f68a764812",
+    "url": "/sharithg/senior-design-SW_miniproject.git/static/media/coronavirus.d8d37e80.png"
+  },
+  {
+    "revision": "3c77962dbecacc3fdcd8d98047fd759d",
+    "url": "/sharithg/senior-design-SW_miniproject.git/static/media/login_register_img.3c77962d.jpg"
+  },
+  {
+    "revision": "81a95320919e4192f75f3619291b5fd3",
+    "url": "/sharithg/senior-design-SW_miniproject.git/static/media/logo.81a95320.png"
+  },
+  {
+    "revision": "fc354215ae198b2521acaee1e11086f8",
+    "url": "/sharithg/senior-design-SW_miniproject.git/static/media/mask.fc354215.jpg"
+  }
+]);
